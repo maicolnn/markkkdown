c69056fa264daaffc5fec3d8ed41e2fc74224e2e
Maicol <maicol.nasimba@epn.edu.ec>
2023-01-08 02:22:27 -0500

    version 2.3

---
+++ file/xd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+    int a,b;
+
+    printf("Escribe un número: ");
+    scanf("%i", a);
+    printf("Escribe otro número")
+    scanf("%i", b)
+
+    if (a>b)
+        printf("El mayor es:%i", a)
+    if (a==b)
+        printf("Los dos números son iguales")
+    if (a<b)
+        printf("El mayor es b: %i", b)
+
+    
+}
